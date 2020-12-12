9521eb27e8f475a245e3553eaf30d54bd2494e42
dreus <leloqu@icloud.com>
2020-12-12 16:29:34 -0500

    se agrego la conexion con el front-end

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ from fastapi import HTTPException
 
 api = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+origins = ["http://127.0.0.1.tiangolo.com", "https://127.0.0.1.tiangolo.com", "http://127.0.0.1:8080"]
+
+api.add_middleware(CORSMiddleware, allow_origins=origins,
+                   allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 @api.post("/user/auth/")
 async def auth_user(user_in: UserIn):
