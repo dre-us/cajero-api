460cd1b49015d3d3a71773e517dd5f0fcd33a5c7
dreus <leloqu@icloud.com>
2020-12-12 12:02:20 -0500

    fix code style

--- db/transaction_db.py
+++ db/transaction_db.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from pydantic import BaseModel
 
+
 class TransactionInDB(BaseModel):
     id_transaction: int = 0
     username: str
@@ -8,8 +9,10 @@ class TransactionInDB(BaseModel):
     value: int
     actual_balance: int
 
+
 database_transactions = []
-generator = {"id":0}
+generator = {"id": 0}
+
 
 def save_transaction(transaction_in_db: TransactionInDB):
     generator["id"] = generator["id"] + 1
--- models/transaction_models.py
+++ models/transaction_models.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel
 from datetime import datetime
 
+
 class TransactionIn(BaseModel):
     username: str
     value: int
+
 
 class TransactionOut(BaseModel):
     id_transaction: int
