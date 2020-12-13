04c51651f6692f78a60c2512818145eb5bfeee2c
dreus <leloqu@icloud.com>
2020-12-12 23:38:24 -0500

    cambio de enlace heroku

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ api = FastAPI()
 
 from fastapi.middleware.cors import CORSMiddleware
 origins = ["http://127.0.0.1.tiangolo.com", "https://127.0.0.1.tiangolo.com",
-           "http://127.0.0.1:8080", "http://127.0.0.1", "https://cajero-app16.herokuapp.com"]
+           "http://127.0.0.1:8080", "http://127.0.0.1", "https://cajero-api4.herokuapp.com/"]
 
 api.add_middleware(CORSMiddleware, allow_origins=origins,
                    allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
